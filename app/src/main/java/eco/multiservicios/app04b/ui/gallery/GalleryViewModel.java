e8a8e5d9c24cc4e6c1294ed39b697a5a65d9cb9b
Gonzalo Neira <ggneiracc68@gmail.com>
2023-10-25 09:38:43 -0500

    v1. Proyecto Nuevo denominado app01

---
+++ app/src/main/java/eco/multiservicios/app04b/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package eco.multiservicios.app04b.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
